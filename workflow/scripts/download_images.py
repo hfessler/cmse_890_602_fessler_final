212df099543aef3a97236501bf5736a66b16d4ca
hfessler <fesslerhenry@gmail.com>
2023-12-13 17:51:07 -0500

    Downloading now renames files to be human readable and have expected output form for snakemake.

--- workflow/scripts/download_images.py
+++ workflow/scripts/download_images.py
@@ -1,14 +1,31 @@
 import pandas as pd
+import os
 from tcia_utils import nbia
+from glob import glob
 
-df = nbia.getSeries(collection=snakemake.params.collection, format="df")
-print(df['PatientID'])
-filtered_df = df.loc[df['PatientID'] == snakemake.params.patient_id]
-scans_df = filtered_df.loc[filtered_df['SeriesDescription'] == snakemake.params.scan]
-masks_df = filtered_df.loc[filtered_df['SeriesDescription'] == snakemake.params.mask]
+for patient_id in snakemake.params.patient_ids:
+    df = nbia.getSeries(collection=snakemake.params.collection, format="df")
+    filtered_df = df.loc[df['PatientID'] == patient_id]
+    scans_df = filtered_df.loc[filtered_df['SeriesDescription'] == snakemake.params.scan]
+    masks_df = filtered_df.loc[filtered_df['SeriesDescription'] == snakemake.params.mask]
 
-scan_uids = scans_df['SeriesInstanceUID'].tolist()
-mask_uids = scans_df['SeriesInstanceUID'].tolist()
+    scan_uids = scans_df['SeriesInstanceUID'].tolist()
+    mask_uids = masks_df['SeriesInstanceUID'].tolist()
 
-nbia.downloadSeries(scan_uids, input_type = "list", path = snakemake.params.scan_storage)
-nbia.downloadSeries(mask_uids, input_type = "list", path = snakemake.params.mask_storage)
+    nbia.downloadSeries(scan_uids, input_type = "list", path = snakemake.params.scan_storage)
+
+    for uid in scan_uids:
+        metadata = nbia.getSeriesMetadata(uid)[0]
+        time = metadata['Study Description'][-1]
+        subject_id = metadata['Subject ID']
+        os.rename(os.path.join(snakemake.params.scan_storage,uid), os.path.join(snakemake.params.scan_storage,f'{subject_id}_{time}'))
+
+    nbia.downloadSeries(mask_uids, input_type = "list", path = snakemake.params.mask_storage)
+
+    for uid in mask_uids:
+        metadata = nbia.getSeriesMetadata(uid)[0]
+        time = metadata['Study Description'][-1]
+        subject_id = metadata['Subject ID']
+        os.rename(os.path.join(snakemake.params.mask_storage,uid), os.path.join(snakemake.params.mask_storage,f'{subject_id}_{time}'))
+
+
