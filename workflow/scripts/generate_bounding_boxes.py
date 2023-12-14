4f3ee917041d2c4eaa19e4849c6ea5abcc94e1ba
hfessler <fesslerhenry@gmail.com>
2023-12-14 14:32:50 -0500

    Improved readability

--- workflow/scripts/generate_bounding_boxes.py
+++ workflow/scripts/generate_bounding_boxes.py
@@ -1,10 +1,17 @@
-from radiomics.imageoperations import checkMask, getMask
-import SimpleITK as sitk
 import numpy as np
+import SimpleITK as sitk
+from radiomics.imageoperations import checkMask
 
-for scan_filename, mask_filename, bounding_box_filename in zip(snakemake.input.scan_filenames, snakemake.input.mask_filenames, snakemake.output.bounding_box_filenames):
+for (scan_filename,
+     mask_filename,
+     bounding_box_filename) in zip(
+         snakemake.input.scan_filenames,
+         snakemake.input.mask_filenames,
+         snakemake.output.bounding_box_filenames):
+
     scan = sitk.ReadImage(scan_filename)
     mask = sitk.ReadImage(mask_filename)
+
     bounding_box, _ = checkMask(scan, mask, label=0)
 
     np.save(bounding_box_filename, bounding_box)
