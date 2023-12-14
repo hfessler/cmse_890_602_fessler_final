2859dd6c4634de254b15012f907c88817be375c8
hfessler <fesslerhenry@gmail.com>
2023-12-14 14:38:25 -0500

    Improved readability, removed unneeded imports, and added comments about processing.

--- workflow/scripts/process_masks.py
+++ workflow/scripts/process_masks.py
@@ -1,12 +1,17 @@
-import SimpleITK as sitk
-import numpy as np
-from radiomics.imageoperations import checkMask, getMask
 import os
+import SimpleITK as sitk
 
 for mask_folder in snakemake.input.mask_folders:
-    maskname = os.path.join(mask_folder, '1-1.dcm')
-    mask = sitk.ReadImage(maskname)
+
+    # ISPY2 masks are segmentation dicom objects,
+    # and are only 1 .dcm file long.
+    mask_filename = os.path.join(mask_folder, '1-1.dcm')
+    mask = sitk.ReadImage(mask_filename)
+
+    # Masks should be cast to 32 bit integers and put in standard
+    # orientation (right to Left, anterior to Posterior, inferior to Superior)
     mask = sitk.Cast(mask, sitk.sitkInt32)
     mask = sitk.DICOMOrient(mask, 'LPS')
-    name = mask_folder+'.nrrd'
-    sitk.WriteImage(mask, name)
+
+    new_filename = mask_folder+'.nrrd'
+    sitk.WriteImage(mask, new_filename)
