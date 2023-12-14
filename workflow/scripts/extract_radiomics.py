704b54e8d49b29f380c8c9fc7824620233be3cd5
hfessler <fesslerhenry@gmail.com>
2023-12-14 14:31:05 -0500

    Fixed bug in code with using wrong mask and improved readability

--- workflow/scripts/extract_radiomics.py
+++ workflow/scripts/extract_radiomics.py
@@ -1,6 +1,6 @@
-import SimpleITK as sitk
 import csv
-from radiomics import featureextractor, getFeatureClasses
+import SimpleITK as sitk
+from radiomics import featureextractor
 
 extractor = featureextractor.RadiomicsFeatureExtractor()
 extractor.loadParams(
@@ -10,18 +10,28 @@ print('Extraction parameters:\n\t', extractor.settings)
 print('Enabled filters:\n\t', extractor.enabledImagetypes)
 print('Enabled features:\n\t', extractor.enabledFeatures)
 
-for cropped_mask_filename, cropped_scan_filename, radiomics_filename in zip(snakemake.input.cropped_mask_filenames, snakemake.input.cropped_scan_filenames, snakemake.output.radiomics_filenames):
+for (cropped_mask_filename,
+     cropped_scan_filename,
+     radiomics_filename) in zip(
+         snakemake.input.cropped_mask_filenames,
+         snakemake.input.cropped_scan_filenames,
+         snakemake.output.radiomics_filenames):
 
     mask = sitk.ReadImage(cropped_mask_filename)
     scan = sitk.ReadImage(cropped_scan_filename)
-    # Remember that masks are provided as inverse masks where 0 indicates tumor masking
+
+    # Remember that masks are provided as inverse masks where 0 indicates tumor masking. We will need to invert this mask ourselves.
     mask_inverted = (mask == 0)
 
-    features = extractor.execute(scan, mask)
+    features = extractor.execute(scan, mask_inverted)
+
+    # features is an ordered dictionary. We will write this output to a csv,
+    # being careful to preserve order.
     with open(radiomics_filename, 'a') as outputFile:
         writer = csv.writer(outputFile, lineterminator='\n')
         headers = list(features.keys())
         writer.writerow(headers)
+
         row = []
         for h in headers:
             row.append(features.get(h, "N/A"))
