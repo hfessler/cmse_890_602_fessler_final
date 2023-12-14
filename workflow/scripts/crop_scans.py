58ac25096543a5340fc46813e4d944fe9d5c3c0f
hfessler <fesslerhenry@gmail.com>
2023-12-14 05:30:17 -0500

    Outputing cropped masks as temporary files for radiomics feature extraction

--- workflow/scripts/crop_scans.py
+++ workflow/scripts/crop_scans.py
@@ -2,9 +2,10 @@ import SimpleITK as sitk
 import numpy as np
 from radiomics.imageoperations import cropToTumorMask
 
-for mask_filename, scan_filename, bounding_box_filename, cropped_scan_filename in zip(snakemake.input.masks, snakemake.input.scans, snakemake.input.bounding_boxes, snakemake.output.cropped_scans):
+for mask_filename, scan_filename, bounding_box_filename, cropped_scan_filename, cropped_mask_filename in zip(snakemake.input.masks, snakemake.input.scans, snakemake.input.bounding_boxes, snakemake.output.cropped_scans, snakemake.output.cropped_masks):
     mask = sitk.ReadImage(mask_filename)
     scan = sitk.ReadImage(scan_filename)
-    bounding_box=np.load(bounding_box_filename)
+    bounding_box = np.load(bounding_box_filename)
     cropped_scan, cropped_mask = cropToTumorMask(scan, mask, bounding_box)
     sitk.WriteImage(cropped_scan, cropped_scan_filename)
+    sitk.WriteImage(cropped_mask, cropped_mask_filename)
