038af0310f0ee479634ce0c6acd4d9e01bbb40c3
hfessler <fesslerhenry@gmail.com>
2023-12-13 20:03:39 -0500

    Split rule so only processing of masks occurs, not along with creation of bounding boxes.

--- workflow/scripts/process_masks.py
+++ workflow/scripts/process_masks.py
@@ -3,18 +3,12 @@ import numpy as np
 from radiomics.imageoperations import checkMask, getMask
 import os
 
-for i, mask_folder in enumerate(snakemake.input.mask_folders):
+for mask_folder in snakemake.input.mask_folders:
     maskname = os.path.join(mask_folder, '1-1.dcm')
-    mask_sitk = sitk.ReadImage(maskname)
-    mask_sitk = sitk.Cast(mask_sitk, sitk.sitkInt32)
-    # ISPY masks are inverse masks, so tumor is marked by label 0
-    mask_inverted = (mask_sitk==0)
-    scan = sitk.ReadImage(snakemake.input.scans[i])
+    mask = sitk.ReadImage(maskname)
+    mask = sitk.Cast(mask, sitk.sitkInt32)
+    mask = sitk.DICOMOrient(mask, 'LPS')
+    name = mask_folder+'.nrrd'
+    sitk.WriteImage(mask, name)
 
-    mask_inverted = sitk.DICOMOrient(mask_inverted, 'LPS')
-    scan = sitk.DICOMOrient(scan, 'LPS')
-
-    bounding_box, _ = checkMask(scan, mask_inverted)
-
-    sitk.WriteImage(mask_sitk, snakemake.output.masks[i])    
-    np.save(snakemake.output.bounding_boxes[i], bounding_box)
+    
