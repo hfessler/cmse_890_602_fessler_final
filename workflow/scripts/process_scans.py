bc4f2f6d00c183c26f9fac99385a197d5d7b7168
hfessler <fesslerhenry@gmail.com>
2023-12-14 14:41:19 -0500

    Improved readability and added comments.

--- workflow/scripts/process_scans.py
+++ workflow/scripts/process_scans.py
@@ -1,11 +1,17 @@
 import SimpleITK as sitk
 
 for scan_folder in snakemake.input.scan_folders:
+
+    # ISPY2 scan images have a variable number of dcm files,
+    # one for each slice of the image
     reader = sitk.ImageSeriesReader()
     dicom_names = reader.GetGDCMSeriesFileNames(scan_folder)
     reader.SetFileNames(dicom_names)
     scan = reader.Execute()
+
+    # Scans should be put in standard orientation (right to Left,
+    # anterior to Posterior, inferior to Superior)
     scan = sitk.DICOMOrient(scan, 'LPS')
 
-    name = scan_folder+'.nrrd'
-    sitk.WriteImage(scan, name)
+    new_filename = scan_folder+'.nrrd'
+    sitk.WriteImage(scan, new_filename)
